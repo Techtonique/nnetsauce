0c0a9c55457017e32e233a771c00692b338a1813
Thierry Moudiki <thierry.moudiki@gmail.com>
2018-11-21 19:51:26 +0100

    add utils for linear/ridge

---
+++ utils/lm_funcs.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy import linalg as la
+import nnetsauce.utils.matrix_ops as mo
+
+
+# in Alphabetical order
+# computes beta_hat = (t(x)%*%x + lam*I)^{-1}%*%t(x)%*%y    
+def beta_hat(x, y, lam = 0.1):
+    # assert on dimensions
+    return np.dot(inv_penalized_cov(x, lam), 
+                      mo.crossprod(x, y))
+
+
+# computes (t(x)%*%x + lam*I)^{-1}
+def inv_penalized_cov(x, lam = 0.1):
+    # assert on dimensions
+    if lam == 0:
+        return la.inv(mo.crossprod(x))
+    else:
+        return la.inv(mo.crossprod(x) + lam*np.eye(x.shape[1]))
