afc5d636821558b43075bbdecc994fdf3eac1499
Thierry Moudiki <thierry.moudiki@gmail.com>
2018-11-22 11:23:47 +0100

    finish init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
-from .lm_funcs import beta_hat, inv_penalized_cov			
-from .matrix_ops import cbind, rbind, crossprod, tcrossprod, scale_matrix, to_np_array
+from .lmfuncs import beta_hat, inv_penalized_cov			
+from .matrixops import cbind, rbind, crossprod, tcrossprod, scale_matrix, to_np_array
 from .misc import merge_two_dicts
-from .psd_check import isPD, nearestPD
+from .psdcheck import isPD, nearestPD
 
 
 __all__ = ["beta_hat", "inv_penalized_cov", "cbind", "rbind", "crossprod", 
