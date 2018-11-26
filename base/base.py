cd12f7a38ba72401cad60e9e01ad4f5eb3d4300e
Thierry Moudiki <thierry.moudiki@gmail.com>
2018-11-26 16:26:31 +0100

    add cases n_clusters == 0 and/or n_hidden_features == 0

--- base/base.py
+++ base/base.py
@@ -10,12 +10,12 @@ from ..simulation import nodesimulation as ns
 class Base(object):
     """Base model with direct link and nonlinear activation
         
-           Parameters
-           ----------
-           n_hidden_features: int
-               number of nodes in the hidden layer
-           activation_name: str
-               activation function: 'relu', 'tanh' or 'sigmoid'
+       Parameters
+       ----------
+       n_hidden_features: int
+           number of nodes in the hidden layer
+       activation_name: str
+           activation function: 'relu', 'tanh' or 'sigmoid'
     """
         
     
@@ -24,7 +24,7 @@ class Base(object):
     def __init__(self, n_hidden_features=5, 
                  activation_name='relu',
                  nodes_sim='sobol',
-                 n_clusters=None,
+                 n_clusters=2,
                  seed = 123):
         
         activation_options = {
@@ -92,6 +92,7 @@ class Base(object):
     
     def fit(self, X, y):
         """Fit training data (X, y).
+        
         Parameters
         ----------
         X : {array-like}, shape = [n_samples, n_features]
@@ -100,10 +101,11 @@ class Base(object):
         n_features is the number of features.
            y : array-like, shape = [n_samples]
                Target values.
-           Returns
-           -------
-           self : object
-         """
+               
+        Returns
+        -------
+        self : object
+        """
         
         centered_y, scaled_Z = self.preproc_training_set(y = y, X = X)
         self.beta = la.lstsq(scaled_Z, centered_y)[0]
@@ -162,8 +164,11 @@ class Base(object):
         
     # create hidden layer
     def create_layer(self, scaled_X, n_features, W=None):        
-            
+        
+        assert scaled_X.shape[1] == n_features
+        
         if (W is None):
+            
             if self.nodes_sim == 'sobol':
                 self.W = ns.generate_sobol(n_dims=n_features, 
                                        n_points=self.n_hidden_features)
@@ -182,7 +187,9 @@ class Base(object):
                                          n_points=self.n_hidden_features)
             
             return self.activation_func(np.dot(scaled_X, self.W))
+        
         else:
+            
             self.W = W
             return self.activation_func(np.dot(scaled_X, W))
         
@@ -192,9 +199,10 @@ class Base(object):
         
         # either X and y are stored or not 
         #assert ((y is None) & (X is None)) | ((y is not None) & (X is not None))
-        nn_scaler = StandardScaler(copy=True, 
-                                    with_mean=True, 
-                                    with_std=True)
+        if self.n_hidden_features > 0:            
+            nn_scaler = StandardScaler(copy=True, 
+                                        with_mean=True, 
+                                        with_std=True)
             
         scaler = StandardScaler(copy=True, 
                                     with_mean=True, 
@@ -215,40 +223,58 @@ class Base(object):
             input_X = X
         
         # 1 - data without clustering: self.n_clusters is None -----      
-        if self.n_clusters is None: 
+        if (self.n_clusters == 0): 
             
             n_features = input_X.shape[1]
-            nn_scaler.fit(input_X)
-            scaled_X = nn_scaler.transform(input_X)
-            self.nn_scaler = nn_scaler
-                        
-            if (W is None):
-                Phi_X = self.create_layer(scaled_X, n_features)
-            else:
-                Phi_X = self.create_layer(scaled_X, n_features, W=W)
             
-            Z = mo.cbind(input_X, Phi_X)
-            scaler.fit(Z)
-            self.scaler = scaler 
-        
+            if self.n_hidden_features > 0:            
+                
+                nn_scaler.fit(input_X)
+                scaled_X = nn_scaler.transform(input_X)
+                self.nn_scaler = nn_scaler
+            
+                if (W is None):
+                    Phi_X = self.create_layer(scaled_X, n_features)
+                else:
+                    Phi_X = self.create_layer(scaled_X, n_features, W=W)
+                
+                Z = mo.cbind(input_X, Phi_X)
+                scaler.fit(Z)
+                self.scaler = scaler 
+                
+            else: # no hidden layer
+                
+                Z = input_X
+                scaler.fit(Z)
+                self.scaler = scaler 
+                
         # 2 - data with clustering: self.n_clusters is not None -----  
         else: 
             
             n_features = input_X.shape[1] + self.n_clusters 
             augmented_X = mo.cbind(input_X, self.encode_kmeans(X = input_X, 
                                                   n_clusters = self.n_clusters))
-            nn_scaler.fit(augmented_X)
-            scaled_X = nn_scaler.transform(augmented_X)           
-            self.nn_scaler = nn_scaler
             
-            if (W is None):
-                Phi_X = self.create_layer(scaled_X, n_features)
-            else:
-                Phi_X = self.create_layer(scaled_X, n_features, W=W)
+            if self.n_hidden_features > 0:
+                
+                nn_scaler.fit(augmented_X)
+                scaled_X = nn_scaler.transform(augmented_X)           
+                self.nn_scaler = nn_scaler
             
-            Z = mo.cbind(augmented_X, Phi_X)
-            scaler.fit(Z)
-            self.scaler = scaler 
+                if (W is None):
+                    Phi_X = self.create_layer(scaled_X, n_features)
+                else:
+                    Phi_X = self.create_layer(scaled_X, n_features, W=W)
+                
+                Z = mo.cbind(augmented_X, Phi_X)
+                scaler.fit(Z)
+                self.scaler = scaler 
+            
+            else: # no hidden layer
+                
+                Z = augmented_X
+                scaler.fit(Z)
+                self.scaler = scaler 
 
         return centered_y, self.scaler.transform(Z) 
     
@@ -262,12 +288,16 @@ class Base(object):
             X = mo.to_np_array(X)
         
         # 1 - data without clustering: self.n_clusters is None -----      
-        if self.n_clusters is None: 
+        if self.n_clusters <= 0: 
             
-            scaled_X = self.nn_scaler.transform(X)
-            Phi_X = self.activation_func(np.dot(scaled_X, self.W))
-           
-            return self.scaler.transform(mo.cbind(X, Phi_X))
+            if self.n_hidden_features > 0:
+                
+                scaled_X = self.nn_scaler.transform(X)
+                Phi_X = self.activation_func(np.dot(scaled_X, self.W))
+               
+                return self.scaler.transform(mo.cbind(X, Phi_X))
+            else:
+                return self.scaler.transform(X)
             
         # 2 - data with clustering: self.n_clusters is None -----      
         else:
@@ -275,15 +305,21 @@ class Base(object):
             augmented_X = mo.cbind(X, self.encode_kmeans(X = X, 
                                                   n_clusters = self.n_clusters, 
                                                   predict = True))
-            scaled_X = self.nn_scaler.transform(augmented_X)
-            Phi_X = self.activation_func(np.dot(scaled_X, self.W))
             
-            return self.scaler.transform(mo.cbind(augmented_X, Phi_X))
+            
+            if self.n_hidden_features > 0:
+                
+                scaled_X = self.nn_scaler.transform(augmented_X)    
+                Phi_X = self.activation_func(np.dot(scaled_X, self.W))
+                
+                return self.scaler.transform(mo.cbind(augmented_X, Phi_X))
+            else:
+                return self.scaler.transform(augmented_X)
             
     
 #if __name__ == '__main__':
 #
-#    from sklearn import datasets   
+    from sklearn import datasets   
 #    
 #    # Example 1 -----
 #    
@@ -313,18 +349,18 @@ class Base(object):
  
 #    # Example 2 -----
     
-#    diabetes = datasets.load_diabetes()
-#    
-#    # data snippet
-#    diabetes.feature_names
-#    
-#    # shape 
-#    diabetes.data.shape
-#    diabetes.target.shape
-#    
-#    # define X and y
-#    X = diabetes.data 
-#    y = diabetes.target
+    diabetes = datasets.load_diabetes()
+    
+    # data snippet
+    diabetes.feature_names
+    
+    # shape 
+    diabetes.data.shape
+    diabetes.target.shape
+    
+    # define X and y
+    X = diabetes.data 
+    y = diabetes.target
 #    
 #    fit_obj = rvflBase(n_hidden_features=3, 
 #                       activation_name='relu', 
