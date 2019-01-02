a497ad0689d01dfec7e1270a5c8b8ee1f2e0de25
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-01-02 21:59:42 +0100

    add function is_factor(y)

--- nnetsauce/utils/__init__.py
+++ nnetsauce/utils/__init__.py
@@ -1,9 +1,9 @@
 from .lmfuncs import beta_hat, inv_penalized_cov			
 from .matrixops import cbind, rbind, crossprod, tcrossprod, scale_matrix, to_np_array
-from .misc import merge_two_dicts
+from .misc import merge_two_dicts, is_factor
 from .psdcheck import isPD, nearestPD
 
 
 __all__ = ["beta_hat", "inv_penalized_cov", "cbind", "rbind", "crossprod", 
            "tcrossprod", "scale_matrix", "to_np_array", "merge_two_dicts",
-           "isPD", "nearestPD"]
+           "is_factor", "isPD", "nearestPD"]
--- nnetsauce/utils/misc.py
+++ nnetsauce/utils/misc.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 # merge two dictionaries
@@ -5,3 +6,20 @@ def merge_two_dicts(x, y):
     z = x.copy()
     z.update(y)
     return z 
+
+
+# check if the response contains only integers
+def is_factor(y):
+    
+    n = len(y)
+    
+    cond0 = (sum(list(map(lambda x: isinstance(y[x], int), 
+                          range(n)))) == n)
+    cond1 = (sum(list(map(lambda x: isinstance(y[x], np.int), 
+                          range(n)))) == n) 
+    cond2 = (sum(list(map(lambda x: isinstance(y[x], np.int32), 
+                          range(n)))) == n) 
+    cond3 = (sum(list(map(lambda x: isinstance(y[x], np.int64), 
+                          range(n)))) == n) 
+    
+    return cond0 or cond1 or cond2 or cond3
