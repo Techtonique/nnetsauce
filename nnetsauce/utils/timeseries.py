c81623fa1567a46be211f3e7a6775ad5c3d6b021
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-01-28 20:46:08 +0100

    do mts tools

---
+++ nnetsauce/utils/timeseries.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+# create k lags for x 
+def create_lags(x, k):
+    
+    n = len(x)
+    n_k = n - k
+    n_col = k + 1
+    res = np.zeros((n_k, n_col))
+    
+    for i in range(n_k):
+        for j in range(n_col):
+            res[i, j] = x[i + j]
+        
+    return res
+
+
+# create inputs for training from MTS (envisage other regressors in X)
+# X in decreasing order (!)   
+def create_train_inputs(X, k):
+
+    n, p = X.shape
+    n_k = n - k 
+    
+    y = np.zeros((n_k, p))
+    xreg = np.zeros((n_k, k*p))
+    
+    for j in range(p):
+        
+        Y_x = create_lags(X[:,j], k)
+    
+        y[:,j] = Y_x[:, 0]
+        
+        for l in range(1, k):
+            
+            xreg[:, j*k + (l-1)] = Y_x[:, l]
+            
+        xreg[:, j*k + (k-1)] = Y_x[:, k]
+            
+    return (y, xreg)
+
+
+# reformat response in prediction loop
+def reformat_response(X, k):
+
+    n, p = X.shape
+    res = np.zeros((1, k*p))
+    
+    for j in range(p):
+        for i in range(k):
+            res[0, j*k + i] = X[i, j]
+            
+    return res
+    
+    
+    
