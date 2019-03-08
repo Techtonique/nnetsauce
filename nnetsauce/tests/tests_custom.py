9c919cce31e89ab0b24fe17e39ae255602357458
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-03-08 20:35:59 +0100

    do tests for score and cross val

--- nnetsauce/tests/tests_custom.py
+++ nnetsauce/tests/tests_custom.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import datasets, linear_model
+from sklearn import datasets, linear_model, gaussian_process
 import unittest as ut
 import nnetsauce as ns
 
@@ -63,6 +63,42 @@ class TestCustom(ut.TestCase):
                         np.allclose(rmse2, 12.968755131423396) & \
                         np.allclose(rmse3, 26.716371782298673) & \
                         np.allclose(rmse4, 33.457280982445447))
+    
+    
+    def test_score(self):
+        
+        np.random.seed(123)
+        X, y = datasets.make_regression(n_samples=15, 
+                                n_features=3)
+        
+        regr = linear_model.BayesianRidge()
+        
+        fit_obj = ns.Custom(obj = regr, n_hidden_features=100, 
+                            direct_link=True, bias=True,
+                            nodes_sim='sobol',
+                            activation_name='relu', n_clusters=2)
+        fit_obj.fit(X, y)
+        
+        self.assertTrue(np.allclose(fit_obj.score(X, y), 
+                                    1.0219738235804167e-09))
+    
+    
+    def test_crossval(self):
+        
+        breast_cancer = datasets.load_breast_cancer()
+        Z = breast_cancer.data
+        t = breast_cancer.target
+        
+        regr = gaussian_process.GaussianProcessClassifier()
+
+        # create objects Custom
+        fit_obj = ns.Custom(obj=regr, n_hidden_features=100, 
+                             direct_link=True, bias=True,
+                             activation_name='relu', n_clusters=0)
+        
+        # 5-fold cross-validation error (classification)
+        self.assertTrue(np.allclose(fit_obj.cross_val_score(Z, t, cv = 5), 
+                         np.array([0.95652174, 0.96521739, 0.97345133, 0.94690265, 0.95575221])))
         
 if __name__=='__main__':
     ut.main()       
