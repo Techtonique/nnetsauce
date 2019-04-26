554e473d74c32b05900b6fd353583a23e9ffd92a
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-04-26 21:14:26 +0200

    do Base

--- nnetsauce/base/base.py
+++ nnetsauce/base/base.py
@@ -1,4 +1,4 @@
-"""Random Vector Functional Link Network regression."""
+"""Base class."""
 
 # Authors: Thierry Moudiki
 #
@@ -52,6 +52,8 @@ class Base(BaseEstimator):
            scaling methods for inputs, hidden layer, and clustering respectively
            (and when relevant). 
            Currently available: standardization ('std') or MinMax scaling ('minmax')
+       col_sample: float
+           percentage of covariates randomly chosen for training    
        seed: int 
            reproducibility seed for nodes_sim=='uniform', clustering and dropout
     """
@@ -70,6 +72,7 @@ class Base(BaseEstimator):
         n_clusters=2,
         type_clust="kmeans",
         type_scaling=("std", "std", "std"),
+        col_sample=1,
         seed=123,
     ):
 
@@ -81,24 +84,26 @@ class Base(BaseEstimator):
             "sigmoid",
             "prelu",
             "elu",
-        ), "'activation_name' should be in ('relu', 'tanh', 'sigmoid','prelu', 'elu')"
+        ), "'activation_name' must be in ('relu', 'tanh', 'sigmoid','prelu', 'elu')"
 
         assert nodes_sim in (
             "sobol",
             "hammersley",
             "uniform",
             "halton",
-        ), "'nodes_sim' should be in ('sobol', 'hammersley', 'uniform', 'halton')"
+        ), "'nodes_sim' must be in ('sobol', 'hammersley', 'uniform', 'halton')"
 
         assert type_clust in (
             "kmeans",
             "gmm",
-        ), "'type_clust' should be in ('kmeans', 'gmm')"
+        ), "'type_clust' must be in ('kmeans', 'gmm')"
 
         assert (len(type_scaling) == 3) & all(
             type_scaling[i] in ("minmax", "std")
             for i in range(len(type_scaling))
         ), "'type_scaling' must have length 3, and available scaling methods are 'minmax' scaling and standardization ('std')"
+        
+        assert (col_sample >= 0) & (col_sample <= 1), "'col_sample' must be comprised between 0 and 1 (both included)"
 
         # activation function -----
 
@@ -123,6 +128,8 @@ class Base(BaseEstimator):
         self.direct_link = direct_link
         self.type_clust = type_clust
         self.type_scaling = type_scaling
+        self.col_sample = col_sample
+        self.index_col = None
         self.n_clusters = n_clusters
         self.clustering_obj = None
         self.clustering_scaler = None
@@ -254,6 +261,8 @@ class Base(BaseEstimator):
                         n_dims=n_features,
                         n_points=self.n_hidden_features,
                     )
+                    
+                assert (scaled_X.shape[1] == self.W.shape[0]), "check dimensions of covariates X and matrix W"
 
                 return mo.dropout(
                     x=self.activation_func(
@@ -263,7 +272,9 @@ class Base(BaseEstimator):
                     seed=self.seed,
                 )
 
-            else:
+            else: # W is not none
+                
+                assert (scaled_X.shape[1] == W.shape[0]), "check dimensions of covariates X and matrix W" 
 
                 # self.W = W
                 return mo.dropout(
@@ -335,7 +346,7 @@ class Base(BaseEstimator):
                     drop_prob=self.dropout,
                     seed=self.seed,
                 )
-                            
+
 
     def cook_training_set(
         self, y=None, X=None, W=None, **kwargs
@@ -370,6 +381,7 @@ class Base(BaseEstimator):
 
         # center y
         if mx.is_factor(y) == False:  # regression
+            
             if y is None:
                 y_mean = self.y.mean()
                 centered_y = self.y - y_mean
@@ -377,11 +389,43 @@ class Base(BaseEstimator):
                 y_mean = y.mean()
                 self.y_mean = y_mean
                 centered_y = y - y_mean
-
+                
         if X is None:
-            input_X = self.X
-        else:
-            input_X = X
+            
+            if self.col_sample == 1:
+                
+                input_X = self.X
+                
+            else:
+                
+                n_features = self.X.shape[1]
+                new_n_features = int(np.ceil(n_features*self.col_sample))
+                assert new_n_features >= 1, "check class attribute 'col_sample' and the number of covariates provided for X"
+                np.random.seed(self.seed)
+                index_col = np.random.choice(range(n_features),
+                                             size = new_n_features,
+                                             replace = False)
+                self.index_col = index_col
+                input_X = self.X[:, self.index_col]
+                
+        else: # X is not None
+            
+            if self.col_sample == 1:
+                
+                input_X = X
+                
+            else:
+                
+                n_features = X.shape[1]
+                new_n_features = int(np.ceil(n_features*self.col_sample))
+                assert new_n_features >= 1, "check class attribute 'col_sample' and the number of covariates provided for X"
+                np.random.seed(self.seed)
+                index_col = np.random.choice(range(n_features),
+                                             size = new_n_features,
+                                             replace = False)
+                self.index_col = index_col
+                input_X = X[:, self.index_col]
+                
 
         if (
             self.n_clusters <= 0
@@ -467,8 +511,16 @@ class Base(BaseEstimator):
             if (
                 self.n_hidden_features > 0
             ):  # if hidden layer
-
-                scaled_X = self.nn_scaler.transform(X)
+                
+                if self.col_sample == 1: 
+                    
+                    scaled_X = self.nn_scaler.transform(X)
+                    
+                else:
+                    
+                    scaled_X = self.nn_scaler.transform(X[:, self.index_col])
+                    
+                    
                 Phi_X = self.create_layer(scaled_X, self.W)
 
                 if self.direct_link == True:
@@ -483,12 +535,23 @@ class Base(BaseEstimator):
                 return self.scaler.transform(X)
 
         else:  # data with clustering: self.n_clusters is None -----
-
-            predicted_clusters = self.encode_clusters(
-                X=X, predict=True, **kwargs
-            )
-            augmented_X = mo.cbind(X, predicted_clusters)
-
+            
+            if self.col_sample == 1: 
+                
+                predicted_clusters = self.encode_clusters(
+                    X=X, predict=True, **kwargs
+                )
+                augmented_X = mo.cbind(X, predicted_clusters)
+                
+            else:
+                
+                predicted_clusters = self.encode_clusters(
+                    X=X[:, self.index_col], predict=True, **kwargs
+                )
+                augmented_X = mo.cbind(X[:, self.index_col], 
+                                       predicted_clusters)
+                
+                
             if (
                 self.n_hidden_features > 0
             ):  # if hidden layer
