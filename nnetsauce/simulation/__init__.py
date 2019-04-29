24b8196b293431aa200a8fd69ba1afae60fa2dea
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-04-29 09:47:08 +0200

    initial working version

--- nnetsauce/simulation/__init__.py
+++ nnetsauce/simulation/__init__.py
@@ -5,10 +5,13 @@ from .nodesimulation import (
     generate_halton,
 )
 
+from .row_subsampling import subsample
+
 
 __all__ = [
     "generate_sobol",
     "generate_uniform",
     "generate_hammersley",
     "generate_halton",
+    "subsample"
 ]
