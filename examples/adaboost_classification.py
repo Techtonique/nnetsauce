6254bc9c044cb685c46954f98de1b86c81399b86
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-08-20 13:31:44 +0200

    add examples of AdaBoost Pt.2

--- examples/adaboost_classification.py
+++ examples/adaboost_classification.py
@@ -17,12 +17,25 @@ t = breast_cancer.target
 np.random.seed(123)
 X_train, X_test, y_train, y_test = train_test_split(Z, t, test_size=0.2)
 
+# SAMME
+clf = LogisticRegression(solver='liblinear', multi_class = 'ovr')
+fit_obj = ns.AdaBoostClassifier(clf, 
+                                n_hidden_features=np.int(56.13806152), 
+                                direct_link=True,
+                                n_estimators=1000, learning_rate=0.09393372,
+                                col_sample=0.52887573, row_sample=0.87781372,
+                                dropout=0.10216064, n_clusters=2,
+                                type_clust="gmm",
+                                verbose=1, seed = 123, 
+                                method="SAMME")  
+fit_obj.fit(X_train, y_train)  
+print(fit_obj.score(X_test, y_test))
+preds = fit_obj.predict(X_test)                        
 
-#iteration 23 -----
-#current minimum:  [56.13806152  0.09393372  0.52887573  0.87781372  0.10216064]
-#current minimum score:  -0.9735042735042736
+print(fit_obj.score(X_test, y_test, scoring="roc_auc"))
+print(metrics.classification_report(preds, y_test))
 
-
+# SAMME.R
 clf = LogisticRegression(solver='liblinear', multi_class = 'ovr')
 fit_obj = ns.AdaBoostClassifier(clf, 
                                 n_hidden_features=np.int(11.22338867), 
