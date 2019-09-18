c9eb7d211638a09df7d82028b9aa3971f51164b4
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-09-18 21:30:13 +0200

    do version 0.2.0

---
+++ CHANGES.md
@@ -0,0 +1,10 @@
+# version 0.2.0
+
+- Add AdaBoost classifier 
+- Add RandomBag classifier (bagging)
+- Remove dependency to package `sobol_seq`(not used)
+
+
+# version 0.1.0
+
+- Initial version
--- setup.py
+++ setup.py
@@ -14,14 +14,15 @@ install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 
 setup(
     name="nnetsauce",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/thierrymoudiki/nnetsauce",
     packages=find_packages(),
     author="Thierry Moudiki",
     author_email="thierry.moudiki@gmail.com",
     description="Machine Learning using combinations of Neural Networks' layers",
     download_url="https://github.com/thierrymoudiki/nnetsauce",
-    install_requires=["numpy >= 1.13.0", "scipy >= 0.19.0", "scikit-learn >= 0.18.0"].append(install_requires),
+    install_requires=["joblib>=0.13.2", "numpy >= 1.13.0", "scipy >= 0.19.0", 
+                      "scikit-learn >= 0.18.0", "tqdm>=4.28.1"].append(install_requires),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
