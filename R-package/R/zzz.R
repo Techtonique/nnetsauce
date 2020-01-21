d724819ee258e1698f7c0c7fa935d14ed2509137
thierrymoudiki <thierry.moudiki@gmail.com>
2020-01-21 13:18:49 +0000

    update onload Pt.5

--- R-package/R/zzz.R
+++ R-package/R/zzz.R
@@ -1,25 +1,43 @@
-# global reference to scipy (will be initialized in .onLoad)
-# numpy <- NULL
-# scipy <- NULL
-# sklearn <- NULL
-# tqdm <- NULL
+# global reference to packages (will be initialized in .onLoad)
+numpy <- NULL
+scipy <- NULL
+sklearn <- NULL
+tqdm <- NULL
 ns <- NULL
 
 
-install_nnetsauce <- function(pip = TRUE) {
+install_packages <- function(pip = TRUE) {
+
+  has_numpy <- reticulate::py_module_available("numpy")
+  has_scipy <- reticulate::py_module_available("scipy")
+  has_sklearn <- reticulate::py_module_available("sklearn")
+  has_tqdm <- reticulate::py_module_available("tqdm")
   has_nnetsauce <- reticulate::py_module_available("nnetsauce")
+
+  if (has_numpy == FALSE)
+    reticulate::py_install("numpy", pip = pip)
+
+  if (has_scipy == FALSE)
+    reticulate::py_install("scipy", pip = pip)
+
+  if (has_sklearn == FALSE)
+    reticulate::py_install("sklearn", pip = pip)
+
+  if (has_tqdm == FALSE)
+    reticulate::py_install("tqdm", pip = pip)
+
   if (has_nnetsauce == FALSE)
-      reticulate::py_install("nnetsauce", pip = pip)
+    reticulate::py_install("nnetsauce", pip = pip)
 }
-install_nnetsauce <- memoise::memoise(install_nnetsauce)
+install_packages <- memoise::memoise(install_packages)
 
 
 .onLoad <- function(libname, pkgname) {
-  do.call("install_nnetsauce", list(pip=TRUE))
-  # use superassignment to update global reference to numpy
-  # numpy <<- reticulate::import("numpy", delay_load = TRUE)
-  # scipy <<- reticulate::import("scipy", delay_load = TRUE)
-  # sklearn <<- reticulate::import("sklearn", delay_load = TRUE)
-  # tqdm <<- reticulate::import("tqdm", delay_load = TRUE)
+  do.call("install_packages", list(pip=TRUE))
+  # use superassignment to update global reference to packages
+  numpy <<- reticulate::import("numpy", delay_load = TRUE)
+  scipy <<- reticulate::import("scipy", delay_load = TRUE)
+  sklearn <<- reticulate::import("sklearn", delay_load = TRUE)
+  tqdm <<- reticulate::import("tqdm", delay_load = TRUE)
   ns <<- reticulate::import("nnetsauce", delay_load = TRUE)
 }
