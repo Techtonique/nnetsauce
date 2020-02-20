b0f9b8e6010590747a7f1bb105bba27470ba43a8
Thierry Moudiki <thierry.moudiki@gmail.com>
2020-02-20 20:42:38 +0100

    example of multitask classifier

---
+++ examples/mtask_classification.py
@@ -0,0 +1,33 @@
+import nnetsauce as ns
+import numpy as np
+from sklearn.datasets import load_breast_cancer, load_wine, load_iris, make_classification
+from sklearn.linear_model import ElasticNet, LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from time import time
+
+
+# dataset no. 1 ---------- 
+
+breast_cancer = load_breast_cancer()
+Z = breast_cancer.data
+t = breast_cancer.target
+np.random.seed(123)
+X_train, X_test, y_train, y_test = train_test_split(Z, t, test_size=0.2)
+
+# Linear Regression is used 
+regr = LinearRegression()
+fit_obj = ns.MtaskClassifier(regr, n_hidden_features=5, 
+                             n_clusters=2, type_clust="gmm")
+
+start = time()
+fit_obj.fit(X_train, y_train)
+print(time() - start)
+
+print(fit_obj.score(X_test, y_test))
+print(fit_obj.score(X_test, y_test, scoring="roc_auc"))
+
+start = time()
+preds = fit_obj.predict(X_test)
+print(time() - start)
+print(metrics.classification_report(preds, y_test))
