2371537830450f10c9c468c4f8efec5cb5261230
Thierry Moudiki <thierry.moudiki@gmail.com>
2020-06-02 00:29:52 +0200

    load dll

--- nnetsauce/utils/where.py
+++ nnetsauce/utils/where.py
@@ -12,7 +12,7 @@ from ctypes import c_double, c_long
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print(f"dir_path where: {dir_path}")
-wherer = ctypes.cdll.LoadLibrary(dir_path + "/wherer.so")  
+wherer = ctypes.cdll.LoadLibrary("nnetsauce/" + dir_path + "/wherer.so")  
 
 def index_where(x, elt):
   
