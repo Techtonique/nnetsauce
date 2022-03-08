fbf5ac7122f47283ec9a1f055206dc1c089e726c
thierrymoudiki <thierry.moudiki@gmail.com>
2022-03-08 17:14:11 +0100

    tests for randombag (regression)

--- nnetsauce/tests/tests_randombag.py
+++ nnetsauce/tests/tests_randombag.py
@@ -1,9 +1,9 @@
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 import unittest as ut
 import nnetsauce as ns
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_breast_cancer, load_wine
+from sklearn.datasets import load_breast_cancer, load_wine, fetch_california_housing
 from sklearn.linear_model import LogisticRegression
 
 
@@ -24,6 +24,12 @@ class TestRandomBag(ut.TestCase):
         Z_train, Z_test, t_train, t_test = train_test_split(
             Z, t, test_size=0.2, random_state=12736
         )
+
+        X2, y2 = fetch_california_housing(return_X_y=True, as_frame=False)
+        # split data into training test and test set
+        X_train2, X_test2, y_train2, y_test2 = train_test_split(X2, y2, 
+                                                    test_size=0.2, random_state=13)
+
 
         clf = DecisionTreeClassifier(max_depth=1, random_state=14235)
         fit_obj = ns.RandomBagClassifier(
@@ -89,6 +95,12 @@ class TestRandomBag(ut.TestCase):
             verbose=0,
         )
 
+        obj = DecisionTreeRegressor(max_depth=3, random_state=123)
+        fit_obj4 = ns.RandomBagRegressor(obj=obj, direct_link=False,
+                             n_estimators=50, 
+                             col_sample=0.9, row_sample=0.9,
+                             dropout=0, n_clusters=0, verbose=1)        
+
         fit_obj.fit(X_train, y_train)
         preds1 = fit_obj.predict_proba(X_test)
 
@@ -97,6 +109,9 @@ class TestRandomBag(ut.TestCase):
 
         fit_obj3.fit(Z_train, t_train)
         preds3 = fit_obj3.predict_proba(Z_test)
+
+        fit_obj4.fit(X_train2, y_train2)  
+        preds4 = fit_obj4.predict(X_test2)
 
         self.assertTrue(np.allclose(preds1[0, 0], 0.043789295499125226))
 
@@ -110,6 +125,8 @@ class TestRandomBag(ut.TestCase):
             np.allclose(fit_obj.predict(X_test)[0], 1)
             & np.allclose(fit_obj2.predict(X_test)[0], 0)
         )
+
+        self.assertTrue(np.allclose(preds4[0], 1.6404241349742967))
 
     def test_score(self):
 
