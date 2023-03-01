464aae57663a37972a4245da0cf3a3b427335fa1
thierrymoudiki <thierry.moudiki@gmail.com>
2023-03-01 18:05:01 +0100

    try using conda_install

--- R-package/R/zzz.R
+++ R-package/R/zzz.R
@@ -7,44 +7,30 @@ sklearn <- NULL
 tqdm <- NULL
 ns <- NULL
 
+# https://rstudio.github.io/reticulate/articles/package.html#installing-python-dependencies
+install_packages <- function(conda = "auto") {
 
-install_miniconda_ <- function(silent = TRUE)
-{
-  try(reticulate::install_miniconda(),
-      silent = silent)
-}
-
-uninstall_nnetsauce <- function(foo = NULL) {
-  python <- reticulate:::.globals$py_config$python
-  packages <- "nnetsauce"
-  args <- c("pip", "uninstall", "--yes", packages)
-  result <- system2(python, args)
-  if (result != 0L) {
-    pkglist <- paste(shQuote(packages), collapse = ", ")
-    msg <- paste("Error removing package(s):", pkglist)
-    stop(msg, call. = FALSE)
-  }
-  packages
-}
-
-install_packages <- function(method = "auto", conda = "auto") {
-
-  reticulate::py_install(packages = c("numpy", "scipy", "six",
-                                      "tqdm", "scikit-learn",
-                                      "nnetsauce"), pip = TRUE,
-  method = method, conda = conda)
-
+  reticulate::conda_install(envname = "r-reticulate",
+                            packages = c("numpy", "scipy", "six",
+                                         "tqdm", "scikit-learn",
+                                         "nnetsauce"),
+                            conda = conda)
 }
 
 
 .onLoad <- function(libname, pkgname) {
 
-  #try(do.call("uninstall_nnetsauce", list(foo=NULL)),
-  #    silent = TRUE)
+  foo <- try(expr = reticulate::use_condaenv("r-reticulate"),
+             silent = FALSE)
+  if (class(foo) == "try-error"){
+    foo <- reticulate::conda_create(envname = "r-reticulate",
+                                    packages = c("numpy", "scipy", "six",
+                                                 "tqdm", "scikit-learn",
+                                                 "nnetsauce"))
+  }
 
-  # do.call("install_miniconda_", list(silent=TRUE))
-
-  do.call("install_packages", list(method = "auto", conda = "auto"))
+  do.call(what = "install_packages",
+          args = list(conda = "auto"))
 
   numpy <<- reticulate::import("numpy", delay_load = TRUE)
   scipy <<- reticulate::import("scipy", delay_load = TRUE)
