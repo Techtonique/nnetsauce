c2139bbbaa0dd8d85c43072bb5acaa7bc630ef27
thierrymoudiki <thierry.moudiki@gmail.com>
2023-03-06 17:53:49 +0100

    try using conda_install

--- R-package/R/zzz.R
+++ R-package/R/zzz.R
@@ -7,30 +7,18 @@ sklearn <- NULL
 tqdm <- NULL
 ns <- NULL
 
-# # https://rstudio.github.io/reticulate/articles/package.html#installing-python-dependencies
-# install_packages <- function(conda = "auto", method = "auto") {
-#
-#   reticulate::py_install(packages = c("numpy", "scipy", "six",
-#                                       "tqdm", "scikit-learn",
-#                                       "nnetsauce"),
-#                          envname = "r-reticulate",
-#                          method = method,
-#                          conda = conda,
-#                          ignore_installed = TRUE)
-# }
 
 .onLoad <- function(libname, pkgname) {
 
   reticulate::virtualenv_create(envname = "r-reticulate",
                                 python = Sys.which("python"))
 
-  reticulate::virtualenv_install(envname = "r-reticulate",
-                                 packages = c("numpy", "scipy", "six",
-                                              "tqdm", "scikit-learn",
-                                              "nnetsauce"))
+  reticulate::conda_install(envname = "r-reticulate",
+                            packages = c("numpy", "scipy", "six",
+                                         "tqdm", "scikit-learn",
+                                         "nnetsauce"))
 
-  # do.call(what = "install_packages",
-  #         args = list(conda = "auto"))
+  reticulate::use_condaenv(condaenv = "r-reticulate")
 
   numpy <<- reticulate::import("numpy", delay_load = TRUE)
   scipy <<- reticulate::import("scipy", delay_load = TRUE)
