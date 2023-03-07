d13feca7e8ab444e7f1903840da7ddfe0e7e6064
thierrymoudiki <thierry.moudiki@gmail.com>
2023-03-07 20:05:18 +0100

    try using conda to install Pt.15

--- R-package/R/zzz.R
+++ R-package/R/zzz.R
@@ -19,10 +19,10 @@ ns <- NULL
 
   reticulate::use_python(Sys.which("python"))
 
-  reticulate::use_virtualenv("~/myenv")
+  #reticulate::use_virtualenv("~/myenv")
+  reticulate::conda_create(envname = "myenv")
 
-  reticulate::use_condaenv("myenv")
-
+  #reticulate::use_condaenv("myenv")
   reticulate::conda_install(envname = "myenv",
                             packages = c("numpy", "scipy",
                                          "six", "scikit-learn",
