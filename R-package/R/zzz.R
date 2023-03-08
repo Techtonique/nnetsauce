eba8b7a036e302e55f9b27ee62eb61f6f1e65b05
thierrymoudiki <thierry.moudiki@gmail.com>
2023-03-08 16:25:28 +0100

    try using conda to install Pt.19

--- R-package/R/zzz.R
+++ R-package/R/zzz.R
@@ -10,24 +10,22 @@ ns <- NULL
 
 .onLoad <- function(libname, pkgname) {
 
-  foo <- try(reticulate::install_miniconda(),
-             silent = FALSE)
-
-  if (class(foo) == "try-error"){
-    message("miniconda already installed")
-  }
-
-  reticulate::use_python(Sys.which("python"))
-
-  reticulate::use_virtualenv("~/myenv")
-
-  reticulate::use_condaenv("myenv")
-
-  reticulate::conda_install(envname = "myenv",
+  foo <- try(reticulate::conda_install(envname = "r-reticulate",
                             packages = c("numpy", "scipy",
                                          "six", "scikit-learn",
                                          "tqdm", "nnetsauce"),
-                            pip = TRUE)
+                            pip = TRUE), silent = FALSE)
+  if (class(foo) == "try-error")
+  {
+    message("Skipping conda_install")
+  }
+
+  foo2 <- try(reticulate::use_condaenv("r-reticulate"),
+              silent = FALSE)
+  if (class(foo2) == "try-error")
+  {
+    message("Skipping use_condaenv")
+  }
 
   numpy <<- reticulate::import("numpy", delay_load = TRUE)
   scipy <<- reticulate::import("scipy", delay_load = TRUE)
