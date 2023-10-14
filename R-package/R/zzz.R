dce55979f5ac9d2c35438194743d4eff91b0502f
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-10-14 20:57:43 +0200

    force install reticulate and nnetsauce Pt.4

--- R-package/R/zzz.R
+++ R-package/R/zzz.R
@@ -5,18 +5,22 @@ sklearn <- NULL
 .onLoad <- function(libname, pkgname) {
   utils::install.packages("reticulate",
                           repos = list(CRAN="https://cloud.r-project.org"))
-  #utils::update.packages("reticulate")
-  # try(reticulate::virtualenv_create("r-reticulate"),
-  #     silent = TRUE)
-  # reticulate::use_virtualenv("r-reticulate")
   reticulate::py_install("scikit-learn",
-                         # envname = "r-reticulate",
                          pip = TRUE,
                          pip_ignore_installed = TRUE)
   reticulate::py_install("nnetsauce",
-                         # envname = "r-reticulate",
                          pip = TRUE,
                          pip_ignore_installed = TRUE)
+  try(reticulate::py_install("scikit-learn",
+                         envname = "r-reticulate",
+                         pip = TRUE,
+                         pip_ignore_installed = TRUE),
+      silent = TRUE)
+  try(reticulate::py_install("nnetsauce",
+                         envname = "r-reticulate",
+                         pip = TRUE,
+                         pip_ignore_installed = TRUE),
+      silent = TRUE)
   # use superassignment to update global reference to package
   ns <<- reticulate::import("nnetsauce", delay_load = TRUE)
   sklearn <<- reticulate::import("sklearn", delay_load = TRUE)
