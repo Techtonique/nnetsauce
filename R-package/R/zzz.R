0993aa961595504314f44d005fe69db3634ecdf2
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-10-14 21:30:11 +0200

    force install reticulate and nnetsauce Pt.5

--- R-package/R/zzz.R
+++ R-package/R/zzz.R
@@ -5,17 +5,22 @@ sklearn <- NULL
 .onLoad <- function(libname, pkgname) {
   utils::install.packages("reticulate",
                           repos = list(CRAN="https://cloud.r-project.org"))
-  reticulate::py_install("scikit-learn",
-                         pip = TRUE,
-                         pip_ignore_installed = TRUE)
+
+  try(reticulate::virtualenv_create('./r-reticulate'),
+      silent = TRUE)
+  try(reticulate::use_virtualenv('./r-reticulate'),
+      silent = TRUE)
+  # reticulate::py_install("scikit-learn",
+  #                        pip = TRUE,
+  #                        pip_ignore_installed = TRUE)
   reticulate::py_install("nnetsauce",
                          pip = TRUE,
                          pip_ignore_installed = TRUE)
-  try(reticulate::py_install("scikit-learn",
-                         envname = "r-reticulate",
-                         pip = TRUE,
-                         pip_ignore_installed = TRUE),
-      silent = TRUE)
+  # try(reticulate::py_install("scikit-learn",
+  #                        envname = "r-reticulate",
+  #                        pip = TRUE,
+  #                        pip_ignore_installed = TRUE),
+  #     silent = TRUE)
   try(reticulate::py_install("nnetsauce",
                          envname = "r-reticulate",
                          pip = TRUE,
