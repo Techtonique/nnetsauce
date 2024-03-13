da52c9f64d9dc48469e0d5d0de301592d36adbee
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-03-13 17:23:46 +0000

    do bayesian custom

---
+++ examples/custom_bayesian_regression.py
@@ -0,0 +1,41 @@
+import os 
+import nnetsauce as ns 
+import numpy as np 
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import BayesianRidge, ARDRegression
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern
+from time import time 
+
+print(f"\n ----- Running: {os.path.basename(__file__)}... ----- \n")
+
+data = load_diabetes()
+X = data.data
+y= data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2, random_state = 123)
+
+regr = ns.CustomRegressor(BayesianRidge())
+start = time()
+regr.fit(X_train, y_train)
+print(f"Elapsed: {time() - start}s")
+preds = regr.predict(X_test, return_std=True)
+print(f"coverage_rate Bayesian Ridge: {np.mean((preds[1]<=y_test)*(preds[2]>=y_test))}")
+
+regr2 = ns.CustomRegressor(ARDRegression())
+start = time()
+regr2.fit(X_train, y_train)
+print(f"Elapsed: {time() - start}s")
+preds2 = regr2.predict(X_test, return_std=True)
+print(f"coverage_rate ARD Regressor: {np.mean((preds2[1]<=y_test)*(preds2[2]>=y_test))}")
+
+regr3 = ns.CustomRegressor(GaussianProcessRegressor(kernel=Matern(nu=1.5),
+                          alpha=1e-6,
+                          normalize_y=True,
+                          n_restarts_optimizer=25,
+                          random_state=42,))
+start = time()
+regr3.fit(X_train, y_train)
+print(f"Elapsed: {time() - start}s")
+preds3 = regr3.predict(X_test, return_std=True)
+print(f"coverage_rate Gaussian Process: {np.mean((preds3[1]<=y_test)*(preds3[2]>=y_test))}")
--- nnetsauce/custom/customRegressor.py
+++ nnetsauce/custom/customRegressor.py
@@ -8,6 +8,7 @@ from .custom import Custom
 from ..utils import matrixops as mo
 from sklearn.base import RegressorMixin
 from functools import partial
+from scipy.stats import norm
 
 
 class CustomRegressor(Custom, RegressorMixin):
@@ -168,7 +169,7 @@ class CustomRegressor(Custom, RegressorMixin):
 
         return self
 
-    def predict(self, X, **kwargs):
+    def predict(self, X, level=95, **kwargs):
         """Predict test data X.
 
         Parameters:
@@ -186,7 +187,43 @@ class CustomRegressor(Custom, RegressorMixin):
 
         """
 
+        if "return_std" in kwargs:
+
+            alpha = 100 - level
+            pi_multiplier = norm.ppf(1 - alpha / 200)
+
+            if len(X.shape) == 1:
+
+                n_features = X.shape[0]
+                new_X = mo.rbind(
+                    X.reshape(1, n_features),
+                    np.ones(n_features).reshape(1, n_features),
+                )
+
+                mean_, std_ = self.obj.predict(
+                        self.cook_test_set(new_X, **kwargs), return_std=True
+                    )[0]
+                
+                preds = self.y_mean_ + mean_                 
+                lower = self.y_mean_ + (mean_ - pi_multiplier*std_)
+                upper = self.y_mean_ + (mean_ + pi_multiplier*std_)
+
+                return preds, lower, upper
+
+            # len(X.shape) > 1
+            mean_, std_ = self.obj.predict(
+                        self.cook_test_set(X, **kwargs), return_std=True
+                    )
+                
+            preds = self.y_mean_ + mean_                 
+            lower = self.y_mean_ + (mean_ - pi_multiplier*std_)
+            upper = self.y_mean_ + (mean_ + pi_multiplier*std_)
+
+            return preds, lower, upper
+
+        # "return_std" not in kwargs
         if len(X.shape) == 1:
+
             n_features = X.shape[0]
             new_X = mo.rbind(
                 X.reshape(1, n_features),
@@ -200,67 +237,7 @@ class CustomRegressor(Custom, RegressorMixin):
                 )
             )[0]
 
+        # len(X.shape) > 1
         return self.y_mean_ + self.obj.predict(
             self.cook_test_set(X, **kwargs), **kwargs
         )
-
-    def score(self, X, y, scoring=None, **kwargs):
-        """ Score the model on test set features X and response y. 
-
-        Parameters: 
-
-            X: {array-like}, shape = [n_samples, n_features]
-                Training vectors, where n_samples is the number 
-                of samples and n_features is the number of features
-
-            y: array-like, shape = [n_samples]
-                Target values
-
-            scoring: str
-                must be in ('explained_variance', 'neg_mean_absolute_error', \
-                    'neg_mean_squared_error', 'neg_mean_squared_log_error', \
-                    'neg_median_absolute_error', 'r2')
-
-            **kwargs: 
-                additional parameters to be passed to scoring functions
-
-        Returns:
-
-            model scores: {array-like}
-
-        """
-
-        preds = self.predict(X)
-
-        if type(preds) == tuple:  # if there are std. devs in the predictions
-            preds = preds[0]
-
-        if scoring is None:
-            scoring = "neg_root_mean_squared_error"
-
-        # check inputs
-        assert scoring in (
-            "explained_variance",
-            "neg_mean_absolute_error",
-            "neg_mean_squared_error",
-            "neg_mean_squared_log_error",
-            "neg_median_absolute_error",
-            "r2",
-            "neg_root_mean_squared_error",
-        ), "'scoring' should be in ('explained_variance', 'neg_mean_absolute_error', \
-                           'neg_mean_squared_error', 'neg_mean_squared_log_error', \
-                           'neg_median_absolute_error', 'r2', 'neg_root_mean_squared_error')"
-
-        scoring_options = {
-            "explained_variance": skm2.explained_variance_score,
-            "neg_mean_absolute_error": skm2.mean_absolute_error,
-            "neg_mean_squared_error": skm2.mean_squared_error,
-            "neg_mean_squared_log_error": skm2.mean_squared_log_error,
-            "neg_median_absolute_error": skm2.median_absolute_error,
-            "r2": skm2.r2_score,
-            "neg_root_mean_squared_error": partial(
-                skm2.mean_squared_error, squared=False
-            ),
-        }
-
-        return scoring_options[scoring](y, preds, **kwargs)
