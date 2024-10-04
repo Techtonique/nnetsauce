1ac5ace4ea0ea0e54d3941e3479ec81b6ee7ed90
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-10-04 02:56:02 +0200

    check custom scores Pt.9

--- nnetsauce/lazypredict/lazydeepMTS.py
+++ nnetsauce/lazypredict/lazydeepMTS.py
@@ -576,10 +576,12 @@ class LazyDeepMTS(MTS):
                     if self.custom_metric:                        
                         try: 
                             custom_metric = self.custom_metric(X_test, X_pred)
+                            print(f"\n\n Custom metric: {custom_metric} \n\n")
                             CUSTOM_METRIC.append(custom_metric)
                         except Exception as e:
                             custom_metric = np.iinfo(np.float32).max
-                            CUSTOM_METRIC.append(np.iinfo(np.float32).max)
+                            print(f"\n\n Custom metric: {custom_metric} \n\n")
+                            CUSTOM_METRIC.append(custom_metric)
 
                     if self.verbose > 0:
                         if (self.replications is not None) or (
@@ -604,9 +606,8 @@ class LazyDeepMTS(MTS):
                             }
 
                         if self.custom_metric:
-                            scores_verbose["Custom metric"] = (
-                                custom_metric
-                            )
+                            scores_verbose["Custom metric"] = custom_metric
+                            
                     if self.predictions:
                         predictions[name] = X_pred
                 except Exception as exception:
@@ -944,9 +945,11 @@ class LazyDeepMTS(MTS):
                                 custom_metric = self.custom_metric(X_test, X_pred)
                             else:
                                 custom_metric = self.custom_metric(X_test_h, X_pred)
+                            print(f"\n\n Custom metric: {custom_metric} \n\n")                            
                             CUSTOM_METRIC.append(custom_metric)
                         except Exception as e:
                             custom_metric = np.iinfo(np.float32).max
+                            print(f"\n\n Custom metric: {custom_metric} \n\n")
                             CUSTOM_METRIC.append(np.iinfo(np.float32).max)
 
                     if self.verbose > 0:
@@ -972,9 +975,7 @@ class LazyDeepMTS(MTS):
                             }
 
                         if self.custom_metric:
-                            scores_verbose["Custom metric"] = (
-                                custom_metric
-                            )
+                            scores_verbose["Custom metric"] = custom_metric                            
 
                         print(scores_verbose)
 
