e8c5ca1efafe80c355286b8d83b92061b229797d
Thierry Moudiki <thierry.moudiki@gmail.com>
2025-01-03 09:07:59 +0100

    add MLP example

--- examples/quantile.py
+++ examples/quantile.py
@@ -22,7 +22,7 @@ datasets = [load_diabetes, fetch_california_housing]
 
 dataset_names = ["diabetes", "california_housing"]
 
-regrs = [RandomForestRegressor(), RidgeCV(), KNeighborsRegressor()]
+regrs = [MLPRegressor(), RandomForestRegressor(), RidgeCV(), KNeighborsRegressor()]
 
 for dataset, dataset_name in zip(datasets, dataset_names):
 
